5fc379344ba7206c4ae30e12d84dd2abff85d6b6
ankith <20mcme19@uohyd.ac.in>
2023-08-03 20:49:59 +0530

    server config setting made

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,15 @@
+const crypto = require('crypto');
+
+function generateRandomSalt() {
+  return crypto.randomBytes(16).toString('base64');
+}
+
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET') || generateRandomSalt(),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT') || generateRandomSalt() ,
   },
   transfer: {
     token: {
